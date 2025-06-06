2bf1a86cfaefd3f33cb0ff313da8b3ddc1cc6b59
Rodrigo Silva <rodrigosalmeidadev@gmail.com>
2025-06-06 18:04:35 -0300

    feat: Adicionadas rotas para o microfrontend, incluindo carregamento dinâmico dos componentes de login e listagem de clientes.

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,21 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule('mf-auth', './Component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'clientes',
+    loadComponent: () =>
+      loadRemoteModule('mf-clientes', './Component').then(
+        (m) => m.ListClientsComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
