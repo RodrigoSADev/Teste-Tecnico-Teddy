6ecdd355886a121753e65b0e7f812cf8a6b3e0f0
Rodrigo Silva <rodrigosalmeidadev@gmail.com>
2025-06-09 17:28:11 -0300

    feat: Criação do componente SelectedClients para exibir e gerenciar clientes selecionados, incluindo funcionalidades para remover seleção e limpar a lista.

---
+++ projects/mf-clientes/src/app/components/selected-clients/selected-clients.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Client } from '../../models/client.interface';
+import { SelectedClientsService } from '../../services/selected-clients.service';
+import { HeaderComponent } from '../../shared/header/header.component';
+
+@Component({
+  selector: 'app-selected-clients.component',
+  imports: [CurrencyPipe, CommonModule, HeaderComponent],
+  templateUrl: './selected-clients.component.html',
+  styleUrl: './selected-clients.component.scss',
+})
+export class SelectedClientsComponent {
+  selectedClientsService = inject(SelectedClientsService);
+  selectedClients = this.selectedClientsService.selectedClients;
+
+  removerSelecao(client: Client) {
+    this.selectedClientsService.toggleSelectClient(client);
+  }
+}
