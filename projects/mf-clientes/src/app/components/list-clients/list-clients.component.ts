854c26a849fcd0cd522855e6c281a2674e9774d0
Rodrigo Silva <rodrigosalmeidadev@gmail.com>
2025-06-09 09:52:16 -0300

    feat: Alterado nome da pasta do header e correção do caminho de importação do HeaderComponent.

--- projects/mf-clientes/src/app/components/list-clients/list-clients.component.ts
+++ projects/mf-clientes/src/app/components/list-clients/list-clients.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from '../../shared/header.component/header.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-list-clients',
