67387cb4f8f96ab821c5852ba85b6c55c2dbc20d
Rodrigo Silva <rodrigosalmeidadev@gmail.com>
2025-06-10 00:10:15 -0300

    fix: Atualização das URLs remotas para os microserviços de autenticação e clientes.

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,8 +1,9 @@
 import { initFederation } from '@angular-architects/native-federation';
 
 initFederation({
-  'mf-auth': 'http://localhost:4202/remoteEntry.json',
-  'mf-clientes': 'http://localhost:4201/remoteEntry.json',
+  'mf-auth': 'https://teste-tecnico-teddy-mf-auth.vercel.app/remoteEntry.json',
+  'mf-clientes':
+    'https://teste-tecnico-teddy-mf-clientes.vercel.app/remoteEntry.jsonn',
 })
   .catch((err) => console.error(err))
   .then((_) => import('./bootstrap'))
