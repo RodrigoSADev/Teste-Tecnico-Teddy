be9ca60dea5e5369aeba466dbe108aceef71d5a0
Rodrigo Silva <rodrigosalmeidadev@gmail.com>
2025-06-10 00:00:16 -0300

    feat: Implementação de função para obter URLs remotas.

--- projects/shell/federation.config.js
+++ projects/shell/federation.config.js
@@ -3,12 +3,32 @@ const {
   shareAll,
 } = require("@angular-architects/native-federation/config");
 
+const getRemoteUrl = (remoteName) => {
+  const env = process.env.NODE_ENV || "development";
+  if (env === "production") {
+    const urls = {
+      "mf-auth":
+        process.env.MF_AUTH_URL ||
+        "https://teste-tecnico-teddy-mf-auth.vercel.app/remoteEntry.json",
+      "mf-clientes":
+        process.env.MF_CLIENTES_URL ||
+        "https://teste-tecnico-teddy-mf-clientes.vercel.app/remoteEntry.json",
+    };
+    return urls[remoteName];
+  }
+  const urls = {
+    "mf-auth": "http://localhost:4202/remoteEntry.json",
+    "mf-clientes": "http://localhost:4201/remoteEntry.json",
+  };
+  return urls[remoteName];
+};
+
 module.exports = withNativeFederation({
   name: "shell",
 
   remotes: {
-    "mf-auth": "http://localhost:4202/remoteEntry.json",
-    "mf-clientes": "http://localhost:4201/remoteEntry.json",
+    "mf-auth": getRemoteUrl("mf-auth"),
+    "mf-clientes": getRemoteUrl("mf-clientes"),
   },
 
   shared: {
